b423a13415f74dc2fbbbad870b4171d31e274f71
johanazlan <johanjibs367@gmail.com>
2021-11-29 13:40:45 +0800

    Updates

---
+++ precache-manifest.57db50567d29047cccc47be30ef023d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49906860bf2921221f617ec79ffbd23c",
+    "url": "/BREW-Feedback-2021/index.html"
+  },
+  {
+    "revision": "8f3a95978ed49943395a",
+    "url": "/BREW-Feedback-2021/static/css/main.886403e3.chunk.css"
+  },
+  {
+    "revision": "f3e78fb2f371ce2c5400",
+    "url": "/BREW-Feedback-2021/static/js/2.f16a2733.chunk.js"
+  },
+  {
+    "revision": "bd96a1cfc47c001e68467155960da745",
+    "url": "/BREW-Feedback-2021/static/js/2.f16a2733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3507c25d34ccbf7fe5e6",
+    "url": "/BREW-Feedback-2021/static/js/3.0199a7af.chunk.js"
+  },
+  {
+    "revision": "8f3a95978ed49943395a",
+    "url": "/BREW-Feedback-2021/static/js/main.8e5ec92b.chunk.js"
+  },
+  {
+    "revision": "0b8f29348c692563ca40",
+    "url": "/BREW-Feedback-2021/static/js/runtime-main.45a228e0.js"
+  }
+]);
