c8df5aaa05ad08ac24f7a27b7b805e98e9c8bade
Johan Azlan <68916754+johanazlan@users.noreply.github.com>
2021-12-14 13:11:15 +0800

    Add files via upload

---
+++ build/precache-manifest.da60f2968d1d6358647be9a2e137367e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b409f897601d0b5cd883361f1735d482",
+    "url": "/BREW-Feedback-2021/index.html"
+  },
+  {
+    "revision": "15bfea9d6365dcf569ba",
+    "url": "/BREW-Feedback-2021/static/css/main.4d65e794.chunk.css"
+  },
+  {
+    "revision": "70a9dad93627447da08e",
+    "url": "/BREW-Feedback-2021/static/js/2.ced0208a.chunk.js"
+  },
+  {
+    "revision": "d55bb155c5a3ed3cad3ce73e073622a0",
+    "url": "/BREW-Feedback-2021/static/js/2.ced0208a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d17512772989c4b094",
+    "url": "/BREW-Feedback-2021/static/js/3.9b924ad7.chunk.js"
+  },
+  {
+    "revision": "15bfea9d6365dcf569ba",
+    "url": "/BREW-Feedback-2021/static/js/main.b9ec930e.chunk.js"
+  },
+  {
+    "revision": "2866e7a91925727c1bec",
+    "url": "/BREW-Feedback-2021/static/js/runtime-main.e46e3002.js"
+  }
+]);
